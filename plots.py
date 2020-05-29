5490c3f9f1ab79829a70089b9b3bbef921084b69
Victor Novosad <v.novosad@ivpa.ru>
2020-05-29 22:51:35 +0300

    coursach

--- plots.py
+++ plots.py
@@ -2,7 +2,7 @@ import os
 from matplotlib import pyplot as plt
 import json
 
-histories_dir = 'histories'
+histories_dir = 'histories/overfitting/'
 
 history_files = [name for name in os.listdir(histories_dir) if 'history' in name]
 parameter_files = [name for name in os.listdir(histories_dir) if 'parameters' in name]
@@ -15,7 +15,10 @@ for name in history_files:
 parameters = []
 for name in parameter_files:
     with open(os.path.join(histories_dir, name), 'r') as f:
-        parameters.append([float(name.split('_')[4])] + json.load(f))
+        params = json.load(f)
+        if params:
+            params = [float(name.split('_')[4])] + params
+        parameters.append(params)
 
 
 plt.figure(figsize=(14, 10))
@@ -26,18 +29,23 @@ legends_acc = sum(
     ],
     [],
 )
-plt.title('model accuracy')
-plt.ylabel('accuracy')
+plt.title('dropout')
+plt.ylabel('dropout')
 plt.xlabel('epoch')
 for i in range(len(histories)):
     # summarize history for accuracy
     color = 'red'
     if 'True' in history_files[i]:
         color = 'blue'
-    plt.plot(histories[i]['val_accuracy'])
+    if 'None' in history_files[i]:
+        color = 'green'
+    # if 'True' in history_files[i]:
+    plt.plot(histories[i]['val_accuracy'], color=color)
+    plt.plot(histories[i]['accuracy'], color=color)
 
-    # plt.plot([j * 5 for j in range(len(parameters[i]))], parameters[i])
+    # if 'True' in history_files[i]:
+    plt.plot([j * 5 for j in range(len(parameters[i]))], parameters[i], color=color)
 
-plt.ylim([0.75, 0.9])
+# plt.ylim([0.7, 1])
 plt.legend(legends_acc, loc='lower right')
 plt.show()
