aa686370ee3776e77949ab0526c5cc62636f0b99
Victor Novosad <v.novosad@ivpa.ru>
2020-06-09 15:22:36 +0300

    dropout gradient works

--- check_gradients.py
+++ check_gradients.py
@@ -1,96 +1,16 @@
-import os
-
 import tensorflow as tf
 from source.classes.dropout import DynamicDropout
 
-train_dataset_url = "https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv"
-train_dataset_fp = tf.keras.utils.get_file(fname=os.path.basename(train_dataset_url),
-                                           origin=train_dataset_url)
-column_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
-feature_names = column_names[:-1]
-label_name = column_names[-1]
-class_names = ['Iris setosa', 'Iris versicolor', 'Iris virginica']
-batch_size = 1
-
-train_dataset = tf.data.experimental.make_csv_dataset(
-    train_dataset_fp,
-    batch_size,
-    column_names=column_names,
-    label_name=label_name,
-    num_epochs=1,
-)
-
-
-def pack_features_vector(features, labels):
-    features = tf.stack(list(features.values()), axis=1)
-
-    return features, labels
-
-
-train_dataset = train_dataset.map(pack_features_vector)
-features, labels = next(iter(train_dataset))
-
 dropout = DynamicDropout(0.5)
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(10, activation=tf.nn.relu, input_shape=(4,)),
-    dropout,
-    tf.keras.layers.Dense(10, activation=tf.nn.relu),
-    tf.keras.layers.Dense(3)
-])
-
-loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
 
-def loss(model, x, y, training):
-    y_ = model(x, training=training)
-
-    return loss_object(y_true=y, y_pred=y_)
-
-
-l = loss(model, features, labels, training=False)
-print("Loss test: {}".format(l))
-
-
-def grad(model, inputs, targets):
+def grad(x):
     with tf.GradientTape() as tape:
-        loss_value = loss(model, inputs, targets, training=True)
+        tape.watch(dropout.rate)
+        out = dropout(x, training=True)
+    return tape.gradient(out, dropout.rate)
 
-    return loss_value, tape.gradient(loss_value, model.trainable_variables)
 
-
-optimizer = tf.keras.optimizers.SGD(learning_rate=0.01)
-
-loss_value, grads = grad(model, features, labels)
-
-print("Step: {}, Initial Loss: {}".format(optimizer.iterations.numpy(),
-                                          loss_value.numpy()))
-
-optimizer.apply_gradients(zip(grads, model.trainable_variables))
-
-print("Step: {},         Loss: {}".format(optimizer.iterations.numpy(),
-                                          loss(model, features, labels, training=True).numpy()))
-
-# Keep results for plotting
-train_loss_results = []
-train_accuracy_results = []
-
-num_epochs = 200
-
-for epoch in range(num_epochs):
-    epoch_loss_avg = tf.keras.metrics.Mean()
-    epoch_accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
-
-    for x, y in train_dataset:
-        loss_value, grads = grad(model, x, y)
-        optimizer.apply_gradients(zip(grads, model.trainable_variables))
-
-        epoch_loss_avg.update_state(loss_value)
-        epoch_accuracy.update_state(y, model(x, training=True))
-
-    train_loss_results.append(epoch_loss_avg.result())
-    train_accuracy_results.append(epoch_accuracy.result())
-
-    if epoch % 1 == 0:
-        for i, var in enumerate(model.trainable_variables):
-            if 'rate' in var.name:
-                print(grads[i])
+print(grad(tf.constant([[3.]])))
+print(grad(tf.constant([[2.]])))
+print(grad(tf.constant([[5., 5.]])))
